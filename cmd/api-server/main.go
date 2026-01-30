c1213a918233bc367db586064c869f749650f364
Logeshwaran N <logeshwaran082@gmail.com>
2026-01-30 17:28:28 +0530

    Add Middleware with Logger to router

--- cmd/api-server/main.go
+++ cmd/api-server/main.go
@@ -5,16 +5,17 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/logeshwarann-dev/news-api-rest/internal/middleware"
 	"github.com/logeshwarann-dev/news-api-rest/internal/router"
 )
 
 func main() {
 
 	log := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{AddSource: true}))
-
-	log.Info("server running on port 8080")
 	r := router.New()
-	if err := http.ListenAndServe(":8080", r); err != nil {
+	wrappedRouter := middleware.AddLogger(log, middleware.LogRequest(r))
+	log.Info("server running on port 8080")
+	if err := http.ListenAndServe(":8080", wrappedRouter); err != nil {
 		log.Error("failed to start server", "error", err)
 	}
 }
