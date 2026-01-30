4018d32825e8053244b2ee8c4094efacbb945b17
Logeshwaran N <logeshwaran082@gmail.com>
2026-01-30 18:45:38 +0530

    Add Validator to check the request

--- internal/model/model.go
+++ internal/model/model.go
@@ -1,13 +1,11 @@
 package model
 
-import "time"
-
 type NewNewsRecord struct {
-	Author    string    `json:"author,omitempty"`
-	Title     string    `json:"title,omitempty"`
-	Summary   string    `json:"summary,omitempty"`
-	Content   string    `json:"content,omitempty"`
-	Source    string    `json:"source,omitempty"`
-	CreatedAt time.Time `json:"createdAt,omitempty"`
-	Tags      []string  `json:"tags,omitempty"`
+	Author    string   `json:"author,omitempty"`
+	Title     string   `json:"title,omitempty"`
+	Summary   string   `json:"summary,omitempty"`
+	Content   string   `json:"content,omitempty"`
+	Source    string   `json:"source,omitempty"`
+	CreatedAt string   `json:"createdAt,omitempty"`
+	Tags      []string `json:"tags,omitempty"`
 }
--- internal/validator/validate.go
+++ internal/validator/validate.go
@@ -1,5 +1,33 @@
 package validator
 
-func ValidateRequest() {
+import (
+	"errors"
+	"fmt"
+	"net/url"
+	"time"
 
+	"github.com/logeshwarann-dev/news-api-rest/internal/model"
+)
+
+func ValidateNewNewsRequest(newsRecord model.NewNewsRecord) (errs error) {
+	if newsRecord.Author == "" {
+		errs = errors.Join(errs, fmt.Errorf("author is empty: %s", newsRecord.Author))
+	}
+	if newsRecord.Title == "" {
+		errs = errors.Join(errs, fmt.Errorf("title is empty: %s", newsRecord.Title))
+	}
+	if newsRecord.Summary == "" {
+		errs = errors.Join(errs, fmt.Errorf("summary is empty: %s", newsRecord.Summary))
+	}
+	if _, err := time.Parse(time.RFC3339, newsRecord.CreatedAt); err != nil {
+		errs = errors.Join(errs, fmt.Errorf("invalid createdAt time: %s", newsRecord.CreatedAt))
+	}
+	if _, err := url.Parse(newsRecord.Source); err != nil {
+		errs = errors.Join(errs, fmt.Errorf("invalid source url: %s", newsRecord.Source))
+	}
+	if len(newsRecord.Tags) == 0 {
+		errs = errors.Join(errs, fmt.Errorf("tags are empty"))
+	}
+
+	return errs
 }
