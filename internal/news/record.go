acbb84d58ae4640f314f44f73d6cd2383d6f3517
Logeshwaran N <logeshwaran082@gmail.com>
2026-02-03 15:50:02 +0530

    Add unittest for FindById DB operation

--- internal/news/record.go
+++ internal/news/record.go
@@ -15,7 +15,7 @@ type Record struct {
 	Summary       string    `bun:"summary,nullzero,notnull"`
 	Content       string    `bun:"content,nullzero,notnull"`
 	Source        string    `bun:"source,nullzero,notnull"`
-	Tags          []string  `bun:"tags,nullzero,notnull"`
+	Tags          []string  `bun:"tags,nullzero,notnull,array"`
 	CreateAt      time.Time `bun:"created_at,nullzero,notnull,default:current_timestamp"`
 	UpdatedAt     time.Time `bun:"updated_at,nullzero,notnull,default:current_timestamp"`
 	DeletedAt     time.Time `bun:"deleted_at,nullzero,soft_delete"`
--- internal/news/store_test.go
+++ internal/news/store_test.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/docker/go-connections/nat"
+	"github.com/google/uuid"
 	"github.com/logeshwarann-dev/news-api-rest/internal/news"
 	"github.com/logeshwarann-dev/news-api-rest/internal/postgres"
 	"github.com/stretchr/testify/assert"
@@ -55,6 +56,19 @@ func TestStore_Create(t *testing.T) {
 			expectedErr:    "not-null",
 			expectedStatus: http.StatusInternalServerError,
 		},
+		{
+			name:    "create_news_success",
+			context: context.Background(),
+			record: news.Record{
+				Author:  "Batman",
+				Title:   "Breaking NEWS",
+				Summary: "A brief summary of news",
+				Content: "Batman is a hero with super powers who saves people from devil",
+				Source:  "https://www.google.com",
+				Tags:    []string{"marvel", "test-1"},
+			},
+			expectedStatus: http.StatusCreated,
+		},
 	}
 	for _, tc := range testcases {
 		t.Run(tc.name, func(t *testing.T) {
@@ -71,6 +85,61 @@ func TestStore_Create(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestStore_FindById(t *testing.T) {
+	testcases := []struct {
+		name           string
+		ctx            context.Context
+		newsId         uuid.UUID
+		expectedRecord news.Record
+		expectedErr    string
+		expectedStatus int
+	}{
+		{
+			name:   "return_valid_news_record",
+			ctx:    context.Background(),
+			newsId: uuid.MustParse("67a7f4b7-2261-4121-a578-bf9da06aa0f3"),
+			expectedRecord: news.Record{
+				Author:  "Batman",
+				Title:   "Breaking NEWS",
+				Summary: "A brief summary of news",
+				Content: "Batman is a hero with super powers who saves people from devil",
+				Source:  "https://www.google.com",
+				Tags:    []string{"marvel", "sci-fi"},
+			},
+			expectedStatus: http.StatusOK,
+		},
+		{
+			name:           "return_not_found_error",
+			ctx:            context.Background(),
+			newsId:         uuid.MustParse("79a7f4b7-2261-4121-a578-bf9da06aa0f3"),
+			expectedErr:    "no rows",
+			expectedStatus: http.StatusNotFound,
+		},
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			s := news.NewStore(db)
+			got, err := s.FindById(tc.ctx, tc.newsId)
+			if tc.expectedErr != "" {
+				assert.Error(t, err)
+				assert.ErrorContains(t, err, tc.expectedErr)
+				var storeErr *news.CustomError
+				assert.ErrorAs(t, err, &storeErr)
+				assert.Equal(t, tc.expectedStatus, storeErr.GetHttpStatus())
+			} else {
+				assert.NoError(t, err)
+				assertOnNews(t, tc.expectedRecord, got)
+			}
+		})
+	}
+
+}
+
+func TestStore_FindAll(t *testing.T) {
+
 }
 
 func assertOnNews(tb testing.TB, expected, got news.Record) {
